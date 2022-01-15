0185663a79bdbd7bde210d02a3025f8d68c54dcd
Bee Clark <mrhumanman2@gmail.com>
2022-01-15 13:41:43 -0500

    Remove Dexie store call from app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -5,11 +5,6 @@ import Form from "./form";
 export default function App() {
 	const [db] = useState(new Dexie("PeopleIndexUserData"));
 
-	db.version(1).stores({
-		users:
-			"++id, name, age, birthDay, birthMonth, pseudonyms, firstMet, lastSpoke"
-	});
-
 	return (
 		<React.StrictMode>
 			<div>
