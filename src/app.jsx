a8b813679ab679f4f8205a38892a42bb4d995445
Bee Clark <mrhumanman2@gmail.com>
2022-01-15 13:41:57 -0500

    Add UserList and UserInfo elements to app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Dexie from "dexie";
 import Form from "./form";
+import UserList from "./userList";
+import UserInfo from "./userInfo";
 
 export default function App() {
 	const [db] = useState(new Dexie("PeopleIndexUserData"));
@@ -8,7 +10,9 @@ export default function App() {
 	return (
 		<React.StrictMode>
 			<div>
+				<UserList list={userList} />
 				<Form db={db} />
+				<UserInfo db={db} />
 			</div>
 		</React.StrictMode>
 	);
