584094b6f2be7ed5eedc5b30a5085f6bf45c8bd3
Bee Clark <mrhumanman2@gmail.com>
2022-01-15 20:05:37 -0500

    Import useEffect in App

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Dexie from "dexie";
 import Form from "./form";
 import UserList from "./userList";
