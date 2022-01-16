ba4732418313ec4819fb1ea5b4afa86313481e91
Bee Clark <mrhumanman2@gmail.com>
2022-01-16 14:05:38 -0500

    Add order to clearData return value

--- src/form.jsx
+++ src/form.jsx
@@ -170,7 +170,19 @@ function clearData(setUser) {
 		firstMetVal: "",
 		lastSpoke: "",
 		lastSpokeVal: "",
-		notes: ""
+		notes: "",
+		order: [
+			"name:::::Name",
+			"pseudonyms:::::Other Names",
+			"age:::::Age",
+			"birthDate:::::Birthday",
+			"religion:::::Religion",
+			"nationality:::::Nationality",
+			"heritage:::::Heritage",
+			"firstMetStr:::::First Met",
+			"lastSpokeStr:::::Last Spoke",
+			"notes:::::Notes"
+		]
 	});
 }
 
