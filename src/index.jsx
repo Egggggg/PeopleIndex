9ca607df1dfb9c605d661dc69f279fc628b71646
Bee Clark <mrhumanman2@gmail.com>
2022-01-16 13:23:19 -0500

    Add ErrorBoundary to gracefully handle errors

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,37 @@ import App from "./app";
 import Dexie from "dexie";
 import * as serviceWorker from "./serviceWorkerRegistration";
 
+class ErrorBoundary extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { error: null, errorInfo: null };
+	}
+
+	render() {
+		if (this.state.errorInfo) {
+			return (
+				<div>
+					<h2>Something went wrong.</h2>
+					<details style={{ whiteSpace: "pre-wrap" }}>
+						{this.state.error && this.state.error.toString()}
+						<br />
+						{this.state.errorInfo.componentStack}
+					</details>
+				</div>
+			);
+		}
+
+		return this.props.children;
+	}
+
+	componentDidCatch(error, errorInfo) {
+		this.setState({
+			error: error,
+			errorInfo: errorInfo
+		});
+	}
+}
+
 async function main() {
 const db = new Dexie("PeopleIndexUserData");
 
