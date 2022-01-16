c0524228d343b07f35f7150632a9cdc986811a46
Bee Clark <mrhumanman2@gmail.com>
2022-01-16 16:14:15 -0500

    Remove `main` function from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -34,33 +34,18 @@ class ErrorBoundary extends React.Component {
 	}
 }
 
-async function main() {
-	const db = new Dexie("PeopleIndexUserData");
+const db = new Dexie("PeopleIndexUserData");
 
-	db.version(4).stores({
-		users:
-			"++id, name, age, birthDay, birthMonth, pseudonyms, firstMet, lastSpoke",
-		settings: "id, dateFormat",
-		defaults: "id, dateFormat"
-	});
+db.version(1).stores({
+	users:
+		"++id, name, age, birthDay, birthMonth, pseudonyms, firstMet, lastSpoke"
+});
 
-	const data = await db.defaults.get({ id: 0 });
-
-	if (!data) {
-		await db.defaults.put({ id: 0, dateFormat: "en_US" });
-		console.log("!data");
-	} else {
-		if (!data.dateFormat) {
-			await db.defaults.put({ ...data, id: 0, dateFormat: "en_US" });
-			console.log("!data.dateFormat");
-		}
-	}
-
-	ReactDOM.render(
-		<React.StrictMode>
-			<ErrorBoundary>
-				<App db={db} />
-			</ErrorBoundary>
-		</React.StrictMode>,
-		document.getElementById("root")
-	);
+ReactDOM.render(
+	<React.StrictMode>
+		<ErrorBoundary>
+			<App db={db} />
+		</ErrorBoundary>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
