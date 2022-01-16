02972288a973fe69e8501eb14a62f47f50fdfa03
Bee Clark <mrhumanman2@gmail.com>
2022-01-16 16:13:48 -0500

    Remove import for serviceWorker

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import Dexie from "dexie";
-import * as serviceWorker from "./serviceWorkerRegistration";
 
 class ErrorBoundary extends React.Component {
 	constructor(props) {
