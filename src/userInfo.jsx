2f026b0fc95c517f4557dc50ca09b9ef23d692a5
Bee Clark <mrhumanman2@gmail.com>
2022-01-16 14:06:06 -0500

    Display all fields in `order` in UserInfo component

--- src/userInfo.jsx
+++ src/userInfo.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function UserInfo({ user }) {
-	return <span>{user.name}</span>;
+	if (Object.keys(user).length === 0) {
+		return <div></div>;
+	}
+
+	const fields = user.order.map((field) => {
+		const [slug, fieldDisplay] = field.split(":::::");
+
+		return (
+			<div key={slug}>
+				<h3>{fieldDisplay}</h3>
+				<p>{user[slug]}</p>
+			</div>
+		);
+	});
+
+	return fields;
 }
