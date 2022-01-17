b5b296e86f6869c851d34bbf58cbbd1e20ce713b
Bee Clark <mrhumanman2@gmail.com>
2022-01-17 14:06:59 -0500

    Import useParams from the right module

--- src/userInfo.jsx
+++ src/userInfo.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useParams, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { selectUser } from "./func";
 
 export default function UserInfo() {
