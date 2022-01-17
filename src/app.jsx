3e86bbfef8f7030bd57af55c1bb8a993f7c80e64
Bee Clark <mrhumanman2@gmail.com>
2022-01-17 12:23:08 -0500

    Move non-component functions into func.js

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Form from "./form";
 import UserList from "./userList";
 import UserInfo from "./userInfo";
+import { refresh, selectUser } from "./func";
 
 const order = [
 	"name:::::Name",
@@ -15,12 +16,6 @@ const order = [
 	"lastSpokeStr:::::Last Spoke",
 	"notes:::::Notes"
 ];
-const dateOptions = {
-	timeZone: "UTC",
-	month: "numeric",
-	day: "numeric",
-	year: "numeric"
-};
 
 export default function App({ db }) {
 	const [users, setUsers] = useState([]);
@@ -51,77 +46,3 @@ export default function App({ db }) {
 		</div>
 	);
 }
-
-const refresh = (setUsers, setSelectedUser, db) => async () => {
-	const now = Date.now();
-	const data = await db.users.toArray();
-
-	const promises = data.map(async (user) => {
-		let sinceSpoke = now - user.lastSpoke;
-
-		sinceSpoke = Math.floor(sinceSpoke / (1000 * 3600 * 24));
-
-		let lastSpoke = new Date(user.lastSpoke);
-
-		lastSpoke = lastSpoke.toLocaleDateString(undefined, dateOptions);
-
-		return (
-			<tr key={user.id}>
-				<td id={`name-${user.id}`}>
-					<button onClick={selectUser(setSelectedUser, db)(user.id)}>
-						{user.name}
-					</button>
-				</td>
-				<td id={`since-${user.id}`}>{sinceSpoke}</td>
-				<td id={`last-${user.id}`}>{lastSpoke}</td>
-				<td id={`delete-${user.id}`}>
-					<button onClick={deleteUser(setUsers, db, user.id)}>Delete</button>
-				</td>
-			</tr>
-		);
-	});
-
-	Promise.all(promises).then((users) => {
-		setUsers(users);
-	});
-};
-
-const selectUser = (setSelectedUser, db) => (userId) => async (e) => {
-	const userData = await db.users.get({ id: userId });
-
-	if (userData.birthMonth && userData.birthDay) {
-		const birthDate = new Date(`${userData.birthMonth}-${userData.birthDay}`);
-
-		userData.birthDate = birthDate.toLocaleDateString(undefined, {
-			timeZone: "UTC",
-			month: "numeric",
-			day: "numeric"
-		});
-	}
-
-	if (userData.firstMet) {
-		const firstMet = new Date(userData.firstMet);
-
-		userData.firstMetStr = firstMet.toLocaleDateString(undefined, dateOptions);
-	}
-
-	if (userData.lastSpoke) {
-		const lastSpoke = new Date(userData.lastSpoke);
-
-		userData.lastSpokeStr = lastSpoke.toLocaleDateString(
-			undefined,
-			dateOptions
-		);
-	}
-
-	if (!userData.order) {
-		await db.users.update(userData.id, { order: order });
-	}
-
-	setSelectedUser(userData);
-};
-
-const deleteUser = (setUsers, db, userid) => async (e) => {
-	await db.users.delete(parseInt(userid));
-	refresh(setUsers, db);
-};
---
+++ src/func.js
@@ -0,0 +1,88 @@
+const dateOptions = {
+	timeZone: "UTC",
+	month: "numeric",
+	day: "numeric",
+	year: "numeric"
+};
+
+export function refresh(setUsers, setSelectedUser, db) {
+	return async () => {
+		const now = Date.now();
+		const data = await db.users.toArray();
+
+		const promises = data.map(async (user) => {
+			let sinceSpoke = now - user.lastSpoke;
+
+			sinceSpoke = Math.floor(sinceSpoke / (1000 * 3600 * 24));
+
+			let lastSpoke = new Date(user.lastSpoke);
+
+			lastSpoke = lastSpoke.toLocaleDateString(undefined, dateOptions);
+
+			return (
+				<tr key={user.id}>
+					<td id={`name-${user.id}`}>
+						<button onClick={selectUser(setSelectedUser, db)(user.id)}>
+							{user.name}
+						</button>
+					</td>
+					<td id={`since-${user.id}`}>{sinceSpoke}</td>
+					<td id={`last-${user.id}`}>{lastSpoke}</td>
+					<td id={`delete-${user.id}`}>
+						<button onClick={deleteUser(setUsers, db, user.id)}>Delete</button>
+					</td>
+				</tr>
+			);
+		});
+
+		Promise.all(promises).then((users) => {
+			setUsers(users);
+		});
+	};
+}
+export function selectUser(setSelectedUser, db, order) {
+	return (userId) => async (e) => {
+		const userData = await db.users.get({ id: userId });
+
+		if (userData.birthMonth && userData.birthDay) {
+			const birthDate = new Date(`${userData.birthMonth}-${userData.birthDay}`);
+
+			userData.birthDate = birthDate.toLocaleDateString(undefined, {
+				timeZone: "UTC",
+				month: "numeric",
+				day: "numeric"
+			});
+		}
+
+		if (userData.firstMet) {
+			const firstMet = new Date(userData.firstMet);
+
+			userData.firstMetStr = firstMet.toLocaleDateString(
+				undefined,
+				dateOptions
+			);
+		}
+
+		if (userData.lastSpoke) {
+			const lastSpoke = new Date(userData.lastSpoke);
+
+			userData.lastSpokeStr = lastSpoke.toLocaleDateString(
+				undefined,
+				dateOptions
+			);
+		}
+
+		if (!userData.order) {
+			await db.users.update(userData.id, { order: order });
+		}
+
+		setSelectedUser(userData);
+	};
+}
+
+function deleteUser(setUsers, db, userid) {
+	return async (e) => {
+		await db.users.delete(parseInt(userid));
+		refresh(setUsers, db);
+	};
+}
