d32b42bcebfb7c7ed13f56f60ea78fdb1d2c7741
Bee Clark <mrhumanman2@gmail.com>
2022-01-18 15:20:06 -0500

    Move default form into func

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -4,104 +4,10 @@ import { getDefaultFields, getDefaultForm, getUserList } from "../func";
 export default function Form({ db, setUsers, order }) {
 	const [user, setUser] = useState(getDefaultFields(order));
 
-	return (
-		<div>
-			<form
-				onSubmit={handleSubmit(db, user, setUser, setUsers, getUserList, order)}
-			>
-				<input
-					type="text"
-					placeholder="Name"
-					name="name"
-					onChange={handleChange(user, setUser)}
-					value={user.name}
-				/>
-				<br />
-				<input
-					type="text"
-					placeholder="Other Names"
-					name="pseudonyms"
-					onChange={handleChange(user, setUser)}
-					value={user.pseudonyms}
-				/>
-				<br />
-				<input
-					type="number"
-					placeholder="Age"
-					name="age"
-					onChange={handleChange(user, setUser)}
-					value={user.age}
-				/>
-				<br />
-				<input
-					type="number"
-					placeholder="Birth Day"
-					name="birthDay"
-					onChange={handleChange(user, setUser)}
-					max="31"
-					min="1"
-					value={user.birthDay}
-				/>
-				<input
-					type="number"
-					placeholder="Birth Month"
-					name="birthMonth"
-					onChange={handleChange(user, setUser)}
-					max="12"
-					min="1"
-					value={user.birthMonth}
-				/>
-				<br />
-				<input
-					type="text"
-					placeholder="Religion"
-					name="religion"
-					onChange={handleChange(user, setUser)}
-					value={user.religion}
-				/>
-				<br />
-				<input
-					type="text"
-					placeholder="Nationality"
-					name="nationality"
-					onChange={handleChange(user, setUser)}
-					value={user.nationality}
-				/>
-				<br />
-				<input
-					type="text"
-					placeholder="Heritage"
-					name="heritage"
-					onChange={handleChange(user, setUser)}
-					value={user.heritage}
-				/>
-				<br />
-				<label htmlFor="firstMet">First Met: </label>
-				<input
-					type="date"
-					name="firstMet"
-					onChange={handleChange(user, setUser)}
-					value={user.firstMetVal}
-				/>
-				<br />
-				<label htmlFor="lastSpoke">Last Spoke: </label>
-				<input
-					type="date"
-					name="lastSpoke"
-					onInput={handleChange(user, setUser)}
-					value={user.lastSpokeVal}
-				/>
-				<br />
-				<textarea
-					placeholder="Notes"
-					name="notes"
-					onChange={handleChange(user, setUser)}
-					value={user.notes}
-				/>
-				<br />
-				<input type="submit" value="Submit" />
-			</form>
-		</div>
+	return getDefaultForm(
+		handleSubmit(db, user, setUser, setUsers, getUserList, order),
+		handleChange(user, setUser),
+		user
 	);
 }
 
