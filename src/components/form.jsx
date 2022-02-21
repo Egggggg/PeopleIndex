f3fe4dbb000d3f2880808e1f9cc3b6d50237db55
Bee Clark <mrhumanman2@gmail.com>
2022-02-21 14:43:29 -0500

    Import getDefault* into Form, remove getUserList

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import {
 	handleSubmit,
 	handleChange,
+	getDefaultFields,
+	getDefaultForm
+} from "../func";
 
 export default function Form({ db, setusers, order }) {
 	const [user, setUser] = useState(getDefaultFields(order));
 
 	return getDefaultForm(
-		handleSubmit(db, user, setUser, setusers, getUserList, order),
+		handleSubmit(db, user, setUser, setusers, order),
 		handleChange(user, setUser),
 		user
 	);
