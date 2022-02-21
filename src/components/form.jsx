0cb9e6d2424baee5c30d7977683f299d3b34d756
Bee Clark <mrhumanman2@gmail.com>
2022-02-21 16:29:03 -0500

    Fix editing

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -12,6 +12,16 @@ export default function Form({ db, setusers, order }) {
 	const [user, setUser] = useState(getDefaultFields(order));
 	const [redirect, setRedirect] = useState(false);
 
+	return (
+		<>
 			{redirect && <Navigate to="/" />}
+			{getDefaultForm(
+				handleSubmit(db, user, setUser, setusers, order, () =>
+					setRedirect(true)
+				),
+				handleChange(user, setUser),
+				user
+			)}
+		</>
 	);
 }
